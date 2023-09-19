073d0dce199976a78bd8b03e9931004623b306ef
Temi Michael <olasehindemichael285@gmail.com>
2023-09-19 18:15:07 +0100

    Comparison

---
+++ operators/comparison.php
@@ -0,0 +1,18 @@
+<?php 
+
+$x = 10;
+$y = 15;
+
+if ($x < $y) {
+    echo "This is True <br>";
+} else {
+    echo "This is false <br>";
+}
+
+// Spaceships
+
+echo 1 <=> 1, "<br>";
+echo 2 <=> 1, "<br>";
+echo 1 <=> 2, "<br>";
+
+?>
