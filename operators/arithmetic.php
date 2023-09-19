e0a4348e15bc8abbce4c9ac62e46b4b0009bdae4
Temi Michael <olasehindemichael285@gmail.com>
2023-09-19 18:03:17 +0100

    Operators

---
+++ operators/arithmetic.php
@@ -0,0 +1,40 @@
+<?php 
+// + - * / 
+
+$x = 8;
+$y = 9;
+$z = 7;
+
+// Addition +
+
+echo $x += 9,'<br>';
+echo $s = $x + $y,'<br>';
+echo $x = $x + 3,'<br>';
+echo $x = $x + $x,'<br>';
+
+// Subtraction -
+
+echo $x -= 9,'<br>';
+echo $s = $x - $y,'<br>';
+echo $x = $x - 3,'<br>';
+echo $x = $x - $x,'<br>';
+
+// Multiplication *
+
+echo $x *= 9,'<br>';
+echo $s = $x * $y,'<br>';
+echo $x = $x * 3,'<br>';
+echo $x = $x * $z,'<br>';
+
+// Division /
+
+echo $z /= 9,'<br>';
+echo $s = $x / $z,'<br>';
+echo $x = $z / 3,'<br>';
+
+
+
+
+
+
+?>
